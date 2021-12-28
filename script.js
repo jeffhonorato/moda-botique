6aa3b3b70ee1323e532e332d129d80eb1183c2d4
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2021-12-28 10:45:17 -0300

    Adicionando JS - Navegação Promoção

--- script.js
+++ script.js
@@ -14,6 +14,28 @@ new Glider(slider, {
       scrollLock: true,
 })
 
-// SLIDER MARCAS
+// PROMOÇÔES 
+const miniaturaPequena = document.querySelectorAll(".js-lista-img img");
+const miniaturaGrande = document.querySelectorAll(".js-lista-conteudo div");
+miniaturaGrande[0].classList.add("ativo");
+
+if(miniaturaPequena.length && miniaturaGrande.length) {
+    const tabImagens = (index) => {
+        miniaturaGrande.forEach((imagem) => {
+            imagem.classList.remove("ativo");
+        })
+    
+        miniaturaGrande[index].classList.add("ativo");
+    }
+    
+    miniaturaPequena.forEach((item, index) => {
+        item.addEventListener("click", () => {
+            tabImagens(index)
+        })
+    })
+}
+
+
+
 
 // const marcas = document.querySelector("")
